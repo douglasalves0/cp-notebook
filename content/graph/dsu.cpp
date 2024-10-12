15cb604e93fdaaea0098806bcc2e56cec0e60674
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2024-10-12 11:20:41 -0300

    adding dsu
    adding dsu

---
+++ content/graph/dsu.cpp
@@ -0,0 +1,16 @@
+#include "../contest/template.cpp"
+
+struct dsu {
+    vector<int> p, rnk;
+    dsu(int n) : p(n), rnk(n, 1) { iota(all(p), 0); }
+    int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
+    bool join(int a, int b) {
+        a = find(a); b = find(b);
+        if (a == b) return false;
+        if (rnk[a] < rnk[b]) swap(a, b);
+        p[b] = a;
+        rnk[a] += rnk[b];
+        return true;
+    }
+    bool same(int a, int b) { return find(a) == find(b); }
+};
