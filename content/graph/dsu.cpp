9444a93471fb810fdb0a1c57b1805efedf0fe86a
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2024-12-13 10:22:25 -0300

    Update dsu.cpp

--- content/graph/dsu.cpp
+++ content/graph/dsu.cpp
@@ -2,7 +2,7 @@
 
 struct dsu {
     vector<int> p, rnk;
-    dsu(int n) : p(n), rnk(n, 1) { iota(all(p), 0); }
+    dsu(int n) : p(n), rnk(n, 1) { iota(p.begin(), p.end(), 0); }
     int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
     bool join(int a, int b) {
         a = find(a); b = find(b);
