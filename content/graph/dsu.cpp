2facad9ea88248b24b95eaf57ee0dde1013b0eba
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2025-01-19 21:20:51 -0300

    Update dsu.cpp

--- content/graph/dsu.cpp
+++ content/graph/dsu.cpp
@@ -1,14 +1,39 @@
-struct dsu {
-    vector<int> p, rnk;
-    dsu(int n) : p(n), rnk(n, 1) { iota(p.begin(), p.end(), 0); }
-    int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
-    bool join(int a, int b) {
-        a = find(a); b = find(b);
-        if (a == b) return false;
-        if (rnk[a] < rnk[b]) swap(a, b);
-        p[b] = a;
-        rnk[a] += rnk[b];
+struct union_find {
+    // When data[x] < 0, x is a root and -data[x] is its tree size. When data[x] >= 0, data[x] is x's parent.
+    vector<int> data;
+    int components = 0;
+ 
+    union_find(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+ 
+    void init(int n) {
+        data.assign(n, -1);
+        components = n;
+    }
+ 
+    int find(int x) {
+        return data[x] < 0 ? x : data[x] = find(data[x]);
+    }
+ 
+    int get_size(int x) {
+        return -data[find(x)];
+    }
+ 
+    bool unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+ 
+        if (x == y)
+            return false;
+ 
+        if (-data[x] < -data[y])
+            swap(x, y);
+ 
+        data[x] += data[y];
+        data[y] = x;
+        components--;
         return true;
     }
-    bool same(int a, int b) { return find(a) == find(b); }
 };
