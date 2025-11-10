8ac0fec74caacddb521a54f581aea0d7d1b46c9d
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2025-11-10 20:45:19 -0300

    Refactor LazySegmentTree implementation

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,52 @@
-/**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
- */
-#pragma once
+struct seg {
+    
+    typedef int TT; // type of tree nodes
+    static constexpr TT unit = 0;
+    vector<TT> tree;
 
-#include "../various/BumpAllocator.h"
+    typedef int LT; // type of lazy nodes
+    static constexpr LT lazy_unit = 0;
+    vector<LT> lazy;
+    
+    int n;
+    seg(int N) {
+        n = N;
+        tree.resize(4*n, unit);
+        lazy.resize(4*n, lazy_unit);
+    }
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+    void push(int no, int l, int r){
+        if(lazy[no] == lazy_unit) return;
+        // update tree[no] using lazy[no]
+        if(l < r){
+            // update lazy[no*2] using lazy[no]
+            // update lazy[no*2+1] using lazy[no]
+        }
+        lazy[no] = lazy_unit;
+    }
+
+    TT f(TT l, TT r){ return l + r; }
+
+    void update(int no, int l, int r, int x, int y, TT v) {
+        push(no, l, r);
+        if(x > y || r < x || y < l) return;
+        if(x <= l && r <= y){
+            // update lazy[no] using v
+            push(no, l, r);
+            return;
+        }
+        int m = (l+r)/2;
+        update(no*2, l, m, x, y, v);
+        update(no*2+1, m+1, r, x, y, v);
+        tree[no] = f(tree[no*2], tree[no*2+1]);
+    }
+    TT query(int no, int l, int r, int x, int y) {
+        push(no, l, r);
+        if(x > y || r < x || y < l) return unit;
+        if(x <= l && r <= y) return tree[no];
+        int m = (l+r)/2;
+        return f(query(no*2, l, m, x, y), query(no*2+1, m+1, r, x, y));
+    }
+    void update(int l, int r, TT v){ update(1, 0, n-1, l, r, v); }
+    TT query(int l, int r) { return query(1, 0, n-1, l, r); }
 };
