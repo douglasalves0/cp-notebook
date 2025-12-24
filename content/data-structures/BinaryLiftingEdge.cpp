cd89ae9ac7073c1687065718b0e63bb38850d863
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2025-12-24 16:02:23 -0300

    Remove dsu struct from BinaryLiftingEdge.cpp
    Removed the dsu struct implementation from BinaryLiftingEdge.cpp.

--- content/data-structures/BinaryLiftingEdge.cpp
+++ content/data-structures/BinaryLiftingEdge.cpp
@@ -80,21 +80,3 @@ struct BinaryLiftingEdge{
         return up[u][0];
     }
 };
-
-struct dsu {
-    vector<int> p, rnk;
-    dsu(int n) : p(n), rnk(n, 1) {
-        iota(p.begin(), p.end(), 0);
-    }
-    int find(int x) {
-        return p[x] == x ? x : p[x] = find(p[x]);
-    }
-    bool join(int a, int b) {
-        a = find(a); b = find(b);
-        if (a == b) return false;
-        if (rnk[a] < rnk[b]) swap(a, b);
-        p[b] = a;
-        rnk[a] += rnk[b];
-        return true;
-    }
-};
