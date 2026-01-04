03588d917dfdb9c882e9b7d0087ed5ca08a5486a
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2026-01-03 22:07:03 -0300

    Refactor Hashing.h by removing comments and cleaning code

--- content/strings/Hashing.h
+++ content/strings/Hashing.h
@@ -1,54 +1,45 @@
-/**
- * Author: Simon Lindholm
- * Date: 2015-03-15
- * License: CC0
- * Source: own work
- * Description: Self-explanatory methods for string hashing.
- * Status: stress-tested
- */
-#pragma once
-
-// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and more
-// code, but works on evil test data (e.g. Thue-Morse, where
-// ABBA... and BAAB... of length 2^10 hash the same mod 2^64).
-// "typedef ull H;" instead if you think test data is random,
-// or work mod 10^9+7 if the Birthday paradox is not a problem.
-typedef uint64_t ull;
-struct H {
-	ull x; H(ull x=0) : x(x) {}
-	H operator+(H o) { return x + o.x + (x + o.x < x); }
-	H operator-(H o) { return *this + ~o.x; }
-	H operator*(H o) { auto m = (__uint128_t)x * o.x;
-		return H((ull)m) + (ull)(m >> 64); }
-	ull get() const { return x + !~x; }
-	bool operator==(H o) const { return get() == o.get(); }
-	bool operator<(H o) const { return get() < o.get(); }
+mt19937 rng((ll) chrono::steady_clock::now().time_since_epoch().count());
+const ll NN = 6e5 + 10, K = 2;
+const ll P = uniform_int_distribution<ll>(1e8, 1e9)(rng);
+const ll MOD[] = {
+    1000893493, 
+    1013782387
 };
-static const H C = (ll)1e11+3; // (order ~ 3e9; random also ok)
-
-struct HashInterval {
-	vector<H> ha, pw;
-	HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
-		pw[0] = 1;
-		rep(i,0,sz(str))
-			ha[i+1] = ha[i] * C + str[i],
-			pw[i+1] = pw[i] * C;
-	}
-	H hashInterval(int a, int b) { // hash [a, b)
-		return ha[b] - ha[a] * pw[b - a];
-	}
-};
-
-vector<H> getHashes(string& str, int length) {
-	if (sz(str) < length) return {};
-	H h = 0, pw = 1;
-	rep(i,0,length)
-		h = h * C + str[i], pw = pw * C;
-	vector<H> ret = {h};
-	rep(i,length,sz(str)) {
-		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
-	}
-	return ret;
+ll pw[NN][K], pre[NN][K], IT; 
+ 
+// SE ESTIVER DANDO PROBLEMA NO CÓDIGO
+// VEJA SE ESTÁ CHAMANDO PRECALC ANTES DE TUDO
+void precalc(){
+    for(ll j=0;j<K;j++) pw[0][j] = 1;
+    for(ll i=1;i<NN;i++){
+        for(ll j=0;j<K;j++){
+            pw[i][j] = pw[i - 1][j] * P % MOD[j];
+        }
+    }
 }
-
-H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
+ 
+struct RollingHash {
+    ll L;
+    RollingHash(const string &s){
+        L = IT, IT += s.length();
+        for(ll j=0;j<K;j++) pre[L][j] = s[0];
+        for(ll i=1;i<s.length();i++){
+            for(ll j=0;j<K;j++){
+                pre[L + i][j] = (pre[L + i - 1][j] * P + s[i]) % MOD[j];
+            }
+        }
+    }
+    array<ll, K> operator()(ll l, ll r){
+        array<ll, K> ans;
+        for(ll j=0;j<K;j++){
+            if(l > r){
+                ans[j] = 0;
+                continue;
+            }
+            ll right = pre[L + r][j], left = 0;
+            if(l) left = pre[L + l - 1][j] * pw[r - l + 1][j] % MOD[j];
+            ans[j] = (right - left + MOD[j]) % MOD[j];
+        }
+        return ans;
+    }
+};
