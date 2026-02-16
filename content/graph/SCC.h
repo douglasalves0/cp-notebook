36eb6c615d4a8dc1ba72e1261495023fe0165211
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2026-02-16 17:45:12 -0300

    Refactor SCC algorithm to use vectors and simplify DFS

--- content/graph/SCC.h
+++ content/graph/SCC.h
@@ -15,27 +15,29 @@
  */
 #pragma once
 
-vi val, comp, z, cont;
-int Time, ncomps;
-template<class G, class F> int dfs(int j, G& g, F& f) {
-	int low = val[j] = ++Time, x; z.push_back(j);
-	for (auto e : g[j]) if (comp[e] < 0)
-		low = min(low, val[e] ?: dfs(e,g,f));
+vi val, comp, z;
+vector<vi> vertices;
+ll Time, ncomps;
+template<class G> ll dfs(ll j, G& g) {
+    ll low = val[j] = ++Time, x; z.push_back(j);
+    for (auto e : g[j]) if (comp[e] < 0)
+        low = min(low, val[e] ?: dfs(e,g));
 
-	if (low == val[j]) {
-		do {
-			x = z.back(); z.pop_back();
-			comp[x] = ncomps;
-			cont.push_back(x);
-		} while (x != j);
-		f(cont); cont.clear();
-		ncomps++;
-	}
-	return val[j] = low;
+    if (low == val[j]) {
+        vertices.push_back(vi());
+        do {
+            x = z.back(); 
+            z.pop_back();
+            comp[x] = ncomps;
+            vertices[ncomps].push_back(x);
+        } while (x != j);
+        ncomps++;
+    }
+    return val[j] = low;
 }
-template<class G, class F> void scc(G& g, F f) {
-	int n = sz(g);
+template<class G> void scc(G& g) {
+	ll n = sz(g);
 	val.assign(n, 0); comp.assign(n, -1);
 	Time = ncomps = 0;
-	rep(i,0,n) if (comp[i] < 0) dfs(i, g, f);
+	rep(i,0,n) if (comp[i] < 0) dfs(i, g);
 }
