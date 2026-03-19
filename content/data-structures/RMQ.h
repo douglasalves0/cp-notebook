2952694ad01e19dbc283d13e3562612dbad645ad
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2026-03-18 23:05:24 -0300

    Refactor RMQ class for improved readability

--- content/data-structures/RMQ.h
+++ content/data-structures/RMQ.h
@@ -15,17 +15,18 @@
 
 template<class T>
 struct RMQ {
-	vector<vector<T>> jmp;
-	RMQ(const vector<T>& V) : jmp(1, V) {
-		for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
-			jmp.emplace_back(sz(V) - pw * 2 + 1);
-			rep(j,0,sz(jmp[k]))
-				jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
-		}
-	}
-	T query(int a, int b) {
-		assert(a < b); // or return inf if a == b
-		int dep = 31 - __builtin_clz(b - a);
-		return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
-	}
+    vector<vector<T>> jmp;
+    RMQ(const vector<T>& V) : jmp(1, V) {
+        for (int pw = 1, k = 1; pw * 2 <= V.size(); pw *= 2, ++k) {
+            jmp.emplace_back(V.size() - pw * 2 + 1);
+            for (int j = 0; j < jmp[k].size(); ++j)
+                jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
+        }
+    }
+    T query(int a, int b) {
+        b++;
+        assert(a < b); // or return inf if a == b
+        int dep = 31 - __builtin_clz(b - a);
+        return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+    }
 };
