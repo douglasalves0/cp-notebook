36bb80eb4b22b67d937b4a0cbb4125f2f44781c7
Douglas Alves <douglas.sousa@ccc.ufcg.edu.br>
2026-04-13 21:48:01 -0300

    Refactor HLD implementation for better clarity and performance

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -1,65 +1,126 @@
-/**
- * Author: Benjamin Qi, Oleksandr Kulkov, chilli
- * Date: 2020-01-12
- * License: CC0
- * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
- * Description: Decomposes a tree into vertex disjoint heavy paths and light
- * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
- * support commutative segtree modifications/queries on paths and subtrees.
- * Takes as input the full adjacency list. VALS\_EDGES being true means that
- * values are stored in the edges, as opposed to the nodes. All values
- * initialized to the segtree default. Root must be 0.
- * Time: O((\log N)^2)
- * Status: stress-tested against old HLD
- */
-#pragma once
+// sample hld code that updates by summing into a range
+// vertices are 0-indexed
+// the root of hld is vertex 0
+//
+// to use this you SHOULD update
+// 1. UNIT
+// 2. LAZY_INVALID
+// 3. f function
+// 4. segtree push function
+// 5. segtree update function
+//
+// usage:
+// HLD<false> hld(adj); to define an HLD with weights on vertices
+// HLD<true> hld(adj);  to define an HLD with weights on edges
 
-#include "../data-structures/LazySegmentTree.h"
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef vector<ll> vi;
 
+typedef ll TT; // type of tree nodes
+static constexpr TT UNIT = 0;
+vector<TT> tree;
+
+static constexpr ll LAZY_INVALID = 0;
+vector<ll> lazy;
+
+// update this function as you wish
+TT f(TT l, TT r){ return l + r; }
+
+struct seg {
+        
+    ll n;
+    seg(ll N) {
+        n = N;
+        tree.resize(4*n, UNIT);
+        lazy.resize(4*n, LAZY_INVALID);
+    }
+
+    void push(ll no, ll l, ll r){
+        if(lazy[no] == LAZY_INVALID) return;
+        // update tree[no] using lazy[no]
+        tree[no] += (r - l + 1) * lazy[no];
+        if(l < r){
+            // update lazy[no*2] using lazy[no]
+            lazy[no*2] += lazy[no];
+            // update lazy[no*2+1] using lazy[no]
+            lazy[no*2+1] += lazy[no];
+        }
+        lazy[no] = LAZY_INVALID;
+    }
+
+    void update(ll no, ll l, ll r, ll x, ll y, TT v) {
+        push(no, l, r);
+        if(x > y || r < x || y < l) return;
+        if(x <= l && r <= y){
+            // update lazy[no] using v
+            lazy[no] += v;
+            push(no, l, r);
+            return;
+        }
+        ll m = (l+r)/2;
+        update(no*2, l, m, x, y, v);
+        update(no*2+1, m+1, r, x, y, v);
+        tree[no] = f(tree[no*2], tree[no*2+1]);
+    }
+    TT query(ll no, ll l, ll r, ll x, ll y) {
+        push(no, l, r);
+        if(x > y || r < x || y < l) return UNIT;
+        if(x <= l && r <= y) return tree[no];
+        ll m = (l+r)/2;
+        return f(query(no*2, l, m, x, y), query(no*2+1, m+1, r, x, y));
+    }
+    void update(ll l, ll r, TT v){ update(1, 0, n-1, l, r, v); }
+    TT query(ll l, ll r) { return query(1, 0, n-1, l, r); }
+};
+
+// This runs a dfs from 0, so your graph should be 0-indexed
+// At the start, all the vertices have the default value of the segtree
 template <bool VALS_EDGES> struct HLD {
-	int N, tim = 0;
+	ll N, tim = 0;
 	vector<vi> adj;
 	vi par, siz, depth, rt, pos;
-	Node *tree;
+    seg tree;
 	HLD(vector<vi> adj_)
 		: N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1), depth(N),
-		  rt(N),pos(N),tree(new Node(0, N)){ dfsSz(0); dfsHld(0); }
-	void dfsSz(int v) {
+		  rt(N),pos(N), tree(N){ dfsSz(0); dfsHld(0); }
+	void dfsSz(ll v) {
 		if (par[v] != -1) adj[v].erase(find(all(adj[v]), par[v]));
-		for (int& u : adj[v]) {
+		for (ll& u : adj[v]) {
 			par[u] = v, depth[u] = depth[v] + 1;
 			dfsSz(u);
 			siz[v] += siz[u];
 			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
 		}
 	}
-	void dfsHld(int v) {
+	void dfsHld(ll v) {
 		pos[v] = tim++;
-		for (int u : adj[v]) {
+		for (ll u : adj[v]) {
 			rt[u] = (u == adj[v][0] ? rt[v] : u);
 			dfsHld(u);
 		}
 	}
-	template <class B> void process(int u, int v, B op) {
+	template <class B> void process(ll u, ll v, B op) {
 		for (; rt[u] != rt[v]; v = par[rt[v]]) {
 			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
-			op(pos[rt[v]], pos[v] + 1);
+			op(pos[rt[v]], pos[v]);
 		}
 		if (depth[u] > depth[v]) swap(u, v);
-		op(pos[u] + VALS_EDGES, pos[v] + 1);
+		op(pos[u] + VALS_EDGES, pos[v]);
 	}
-	void modifyPath(int u, int v, int val) {
-		process(u, v, [&](int l, int r) { tree->add(l, r, val); });
+	void updatePath(ll u, ll v, TT val) {
+		process(u, v, [&](ll l, ll r) { tree.update(l, r, val); });
 	}
-	int queryPath(int u, int v) { // Modify depending on problem
-		int res = -1e9;
-		process(u, v, [&](int l, int r) {
-				res = max(res, tree->query(l, r));
-		});
+	TT queryPath(ll u, ll v){
+		TT res = UNIT;
+		process(u, v, [&](ll l, ll r) { res = f(res, tree.query(l, r)); });
 		return res;
 	}
-	int querySubtree(int v) { // modifySubtree is similar
-		return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
+	TT querySubtree(ll v){
+		return tree.query(pos[v] + VALS_EDGES, pos[v] + siz[v] - 1);
 	}
+    void updateSubtree(ll v, TT val){
+        tree.update(pos[v] + VALS_EDGES, pos[v] + siz[v] - 1, val);
+    }
 };
